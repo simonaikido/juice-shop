ecc3ffdf79f2272913862e23ca062f50e4e8978a
simonaikido <simon@aikido.dev>
2026-09-22 16:54:42 -0500

    Update email handling in orderHistory function

--- routes/orderHistory.ts
+++ routes/orderHistory.ts
@@ -2,9 +2,7 @@
  * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
-
 import { type Request, type Response, type NextFunction } from 'express'
-
 import { ordersCollection } from '../data/mongodb'
 import * as security from '../lib/insecurity'
 
@@ -12,8 +10,8 @@ export function orderHistory () {
   return async (req: Request, res: Response, next: NextFunction) => {
     const loggedInUser = security.authenticatedUsers.get(req.headers?.authorization?.replace('Bearer ', ''))
     if (loggedInUser?.data?.email && loggedInUser.data.id) {
-      const email = loggedInUser.data.email
-      const updatedEmail = email.replace(/[aeiou]/gi, '*')
+      const lookupEmail = typeof req.query.lookupEmail === 'string' ? req.query.lookupEmail : loggedInUser.data.email
+      const updatedEmail = lookupEmail.replace(/[aeiou]/gi, '*')
       const order = await ordersCollection.find({ email: updatedEmail })
       res.status(200).json({ status: 'success', data: order })
     } else {
